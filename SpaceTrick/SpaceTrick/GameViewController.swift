cd1a0a137111dc3d53b7e27cfbfb0cc7ed707915
Kim SAVAROCHE <kim.savaroche@hins.fr>
2017-03-19 15:43:07 +0100

    show gyroscope data

--- SpaceTrick/SpaceTrick/GameViewController.swift
+++ SpaceTrick/SpaceTrick/GameViewController.swift
@@ -9,97 +9,104 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import CoreMotion
 
 class GameViewController: UIViewController {
 
+    var currentMaxAccelX: Double = 0.0
+    var currentMaxAccelY: Double = 0.0
+    var currentMaxAccelZ: Double = 0.0
+    var currentMaxRotX: Double = 0.0
+    var currentMaxRotY: Double = 0.0
+    var currentMaxRotZ: Double = 0.0
+    
+    var motionManager = CMMotionManager()
+    
+    // MARK: pragma mark - Outlets
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        motionManager.gyroUpdateInterval = 0.2
+        motionManager.accelerometerUpdateInterval = 0.2
         
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
+        //Start Recording Data
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (accelerometerData: CMAccelerometerData?, NSError) -> Void in
+            
+            self.outputAccData(acceleration: accelerometerData!.acceleration)
+            if(NSError != nil) {
+                print("\(NSError)")
+            }
+        }
         
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+        /*
+        motionManager.startGyroUpdates(to: OperationQueue.current!, withHandler: { (gyroData: CMGyroData?, NSError) -> Void in
+            self.outputRotData(rotation: gyroData!.rotationRate)
+            if (NSError != nil){
+                print("\(NSError)")
+            }
+        })
+         */
     }
     
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
+    func outputAccData(acceleration: CMAcceleration){
+        // print( "Acc X : \(acceleration.x).2fg" )
+        // print( "Acc Y : \(acceleration.y).2fg" )
+        // print( "Acc Z : \(acceleration.z).2fg" )
         
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result: AnyObject = hitResults[0]
-            
-            // get its material
-            let material = result.node!.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
+        print(String(format: "Y variation : %.2f", acceleration.y))
+        
+        print( "======================" )
+        
+        /*
+         if fabs(acceleration.x) > fabs(currentMaxAccelX)
+         {
+         currentMaxAccelX = acceleration.x
+         }
+         if fabs(acceleration.y) > fabs(currentMaxAccelY)
+         {
+         currentMaxAccelY = acceleration.y
+         }
+         if fabs(acceleration.z) > fabs(currentMaxAccelZ)
+         {
+         currentMaxAccelZ = acceleration.z
+         }
+         
+        maxAccX?.text = "\(currentMaxAccelX).2f"
+        maxAccY?.text = "\(currentMaxAccelY).2f"
+        maxAccZ?.text = "\(currentMaxAccelZ).2f"
+        */
     }
+    
+    func outputRotData(rotation: CMRotationRate){
+        print( "Rot X : \(rotation.x).2fr/s" )
+        print( "Rot Y : \(rotation.y).2fr/s" )
+        print( "Rot Z : \(rotation.z).2fr/s" )
+        
+        /*
+        if fabs(rotation.x) > fabs(currentMaxRotX) {
+            currentMaxRotX = rotation.x
+        }
+        
+        if fabs(rotation.y) > fabs(currentMaxRotY) {
+            currentMaxRotY = rotation.y
+        }
+        
+        if fabs(rotation.z) > fabs(currentMaxRotZ) {
+            currentMaxRotZ = rotation.z
+        }
+        
+        
+        maxRotX?.text = "\(currentMaxRotX).2f"
+        maxRotY?.text = "\(currentMaxRotY).2f"
+        maxRotZ?.text = "\(currentMaxRotZ).2f"
+         */
+        
+    }
+    
+    
+    
+    
     
     override var shouldAutorotate: Bool {
         return true
